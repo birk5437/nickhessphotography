26f590c01506fc315ef8b196f0f3283e8967b085
Burke Ramsey <birk5437@gmail.com>
2014-03-28 22:35:22 -0400

    remove aws keys

--- upload_nicks_website.sh
+++ upload_nicks_website.sh
@@ -1,4 +1,4 @@
 #bin/bash
-export AWS_ACCESS_KEY_ID=AKIAIYFJXCDT7IRCFUHQ
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
 s3sync -r -p --progress /Users/burke/Desktop/nicks_website nicks_website:
